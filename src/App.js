70d806a71f67175e901114b2ac3daf9cd61adbe9
KniteDev <bmose@atbtechsoft.com>
2018-10-28 20:49:56 +0100

    Create State and setState using events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Section from './Section';
+import AddFriends from './AddFriends';
 
 class App extends Component {
+
+  state = {
+   username : 'Ayotunde'
+  }
+
+  onhandleInsert = (e) => {
+    this.setState({
+      username: e.target.value,
+      friends: ['issac', 'ebuka', 'joe']
+    })
+  
+  }
+
+
   render() {
+    const {  
+      onhandleInsert,
+      state: { username }
+    } = this;
+   
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Section Username={username}/>
+          <img src={logo}  className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <AddFriends Username={username} handleInsert={onhandleInsert}/>
           <a
             className="App-link"
             href="https://reactjs.org"
